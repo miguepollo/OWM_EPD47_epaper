1950bc51651ecfaf1e7127ea31573350a307bd89
miguepollo <miguelgarciapel@protonmail.com>
2023-03-29 18:07:55 +0200

    put things in spanish because its language in spanish.

--- lang_es.h
+++ lang_es.h
@@ -1,23 +1,23 @@
 #define FONT(x) x##_tf
 
-//Temperature - Humidity - Forecast
-const String TXT_FORECAST_VALUES  = "3-Day Forecast Values";
-const String TXT_CONDITIONS       = "Conditions";
-const String TXT_DAYS             = "(Days)";
+//Temperatura - Humedad - previsión
+const String TXT_FORECAST_VALUES  = "Prevision de 3 dias";
+const String TXT_CONDITIONS       = "Condiciones";
+const String TXT_DAYS             = "(Dias)";
 const String TXT_TEMPERATURES     = "Temperatura";
 const String TXT_TEMPERATURE_C    = "Temperatura (°C)";
 const String TXT_TEMPERATURE_F    = "Temperatura (°F)";
 const String TXT_HUMIDITY_PERCENT = "Humedad (%)";
 
-// Pressure
+// Presión
 const String TXT_PRESSURE         = "Presión";
 const String TXT_PRESSURE_HPA     = "Presión (hPa)";
 const String TXT_PRESSURE_IN      = "Presión (in)";
-const String TXT_PRESSURE_STEADY  = "Steady";
-const String TXT_PRESSURE_RISING  = "Rising";
-const String TXT_PRESSURE_FALLING = "Falling";
+const String TXT_PRESSURE_STEADY  = "Constante";
+const String TXT_PRESSURE_RISING  = "Aumentando";
+const String TXT_PRESSURE_FALLING = "Callendo";
 
-//RainFall / SnowFall
+//Lluvia / Nieve
 const String TXT_RAINFALL_MM = "Lluvia (mm)";
 const String TXT_RAINFALL_IN = "Lluvia (in)";
 const String TXT_SNOWFALL_MM = "Nieve (mm)";
@@ -25,11 +25,11 @@ const String TXT_SNOWFALL_IN = "Nieve (in)";
 const String TXT_PRECIPITATION_SOON = "Prec.";
 
 
-//Sun
+//Sol
 const String TXT_SUNRISE  = "Amanecer";
 const String TXT_SUNSET   = "Atardecer";
 
-//Moon
+//luna
 const String TXT_MOON_NEW             = "Nueva";
 const String TXT_MOON_WAXING_CRESCENT = "Creciente";
 const String TXT_MOON_FIRST_QUARTER   = "Creciente";
@@ -39,13 +39,13 @@ const String TXT_MOON_WANING_GIBBOUS  = "Menguante";
 const String TXT_MOON_THIRD_QUARTER   = "Menguante";
 const String TXT_MOON_WANING_CRESCENT = "Menguante";
 
-//Power / WiFi
-const String TXT_POWER  = "Power";
+//Energia / WiFi
+const String TXT_POWER  = "Energía";
 const String TXT_WIFI   = "WiFi";
-const char* TXT_UPDATED = "Updated:";
+const char* TXT_UPDATED = "Actualizado:";
 
 
-//Wind
+//Viento
 const String TXT_WIND_SPEED_DIRECTION = "Wind Speed/Direction";
 const String TXT_N   = "N";
 const String TXT_NNE = "NNE";
@@ -64,7 +64,7 @@ const String TXT_WNW = "WNW";
 const String TXT_NW  = "NW";
 const String TXT_NNW = "NNW";
 
-//Day of the week
+//Dia de la semana
 const char* weekday_D[] = { "Domingo", "Lunes", "Martes", "Miércoles","Jueves", "Viernes", "Sabado" };
 
 //Mes
